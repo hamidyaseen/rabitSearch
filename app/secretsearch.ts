5ea7811a60ee8e7968f3135dc833bbe69cccd2b5
Hamid Yaseen <hamid.yaseen@teo-intl.com>
2021-02-16 23:15:15 +0100

    removed some dead/commented out  code

--- app/secretsearch.ts
+++ app/secretsearch.ts
@@ -27,9 +27,7 @@ export interface ISubRange {
 export class SecretSearch implements ISecretSearch {
     private _anagram: string = '';
     private _anaLet: string = '';
-    //private wordsList: IWordsList = [];
-    //private _wordsLengths: number[] = [];
-
+   
     constructor() { }
     set anagram(an: string) {
         if (an !== undefined || an !== null || an !== "") {
@@ -79,22 +77,7 @@ export class SecretSearch implements ISecretSearch {
 
         return Math.sqrt(sum / sampleData.length);
     }
-
-    //public canStartEdge(words: IWordsList, i: number, anagram: string): number {
-    //    let maxLength = anagram.length - 2;
-    //    let maxIndex = words.length-1;
-    //    try {
-    //        let restLen = maxLength - i;            
-    //    }
-    //    catch (e) {
-    //    }
-    //}
-    //public checkEdgeCases(words: IWordsList, indexArray: number[]): number[] {
-    //    let firstIndex = indexArray[0], secondIndex = indexArray[1], thirdIndex = indexArray[2];
-    //    if (parseInt(firstIndex.toString()) === NaN || parseInt(secondIndex.toString()) === NaN || parseInt(thirdIndex.toString()) === NaN)
-    //        return [];
-    //    return [];
-    //}
+    
     
     public searchWordInSubset(words: IWordsList, hash: string, range: ISubRange): string
     {
@@ -166,56 +149,6 @@ export class SecretSearch implements ISecretSearch {
         return result;
     }
 
-    //public searchWordDouble(words: IWordsList, hash: string): string {
-    //    let result = "";
-    //    let len = words.length - 1;
-    //    let anagramLen = this.anagram.length - 2; // 2 are space character
-    //    //console.log(len + ' ' + anagramLen);
-    //    let testCombine = [];
-
-    //    let aCombination = '', bCombination;
-    //    try {
-    //        for (let i = len; i > 0; i--) {
-    //            let restLen = anagramLen - i;
-    //            testCombine = []; 
-    //            for (let j = 1, k = (restLen - 1); (j < restLen && k > 0); j++, k--) {
-    //                let aCombine = parseInt([j, k].join(""));
-    //                if (testCombine.find((value: number) => value == aCombine) === undefined) {
-    //                    testCombine.push(aCombine);
-    //                    aCombine = parseInt([k, j].join(""));
-    //                    if (testCombine.find((value: number) => value == aCombine) === undefined)
-    //                        testCombine.push();
-
-    //                    for (let l = 0; l < words[i].length; l++) {
-    //                        for (let m = 0; m < words[j].length; m++) {
-    //                            for (let n = 0; n < words[k].length; n++)
-    //                            {
-    //                                aCombination = [words[i][l], words[j][m], words[k][n]].join(' ');
-    //                                if (Md5.hashStr(aCombination) === hash) {
-    //                                    result = aCombination;                                        
-    //                                    return result;
-    //                                }
-    //                                aCombination = [words[i][l], words[k][n], words[j][m]].join(' ');
-    //                                if (Md5.hashStr(aCombination) === hash) {
-    //                                    result = aCombination;
-    //                                    return result;
-    //                                }
-    //                            }
-    //                        }
-    //                    }
-    //                } 
-                        
-    //            }
-    //            console.log('Next Step Length ' + i);
-    //        }
-    //    }
-    //    catch (e) {
-    //        console.log(e);
-    //    }
-        
-    //    console.log('test finished');
-    //    return result;
-    //}
 
     private uniqueAnaLet(): string {
         let a = this.anagram.split(' ').join('');
